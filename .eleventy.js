5064453e62b5b7654b15be3dd0a93f52252fbf26
c0ffeeca7 <g@briela.ch>
2022-06-05 17:09:16 +0200

    deduplicate installing-ha procedure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const yaml = require("js-yaml");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
   return {
     dir: {
       input: "src",
