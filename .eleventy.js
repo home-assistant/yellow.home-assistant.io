f4c8d8eb0e98ead6836fc860410209c480e1ef44
Stefan Agner <stefan@agner.ch>
2022-10-13 13:40:18 +0200

    Use 11ty to deploy images/resources
    This allows to add images in the source directory 11ty will deploy
    them without restarting script/develop.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addPassthroughCopy(
+    {
+      "public/images": "images",
+      "public/resources": "resources",
+    }
+  );
   eleventyConfig.addFilter("sortByOrder", (arr) => {
     if (!arr) return [];
     arr.sort((a, b) => (a.data.order > b.data.order ? 1 : -1));
