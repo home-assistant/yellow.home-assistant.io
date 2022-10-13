1f5dda31a48813411333007c880e657bccc01d6f
Stefan Agner <stefan@agner.ch>
2022-10-13 18:27:41 +0200

    Add Pinion (KiCad generated hardware documentation)

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
     {
       "public/images": "images",
       "public/resources": "resources",
+      "public/pinion": "pinion",
     }
   );
   eleventyConfig.addFilter("sortByOrder", (arr) => {
